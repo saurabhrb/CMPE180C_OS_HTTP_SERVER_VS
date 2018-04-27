7cf5ff86c179561ca82a8bcd348e50931614d127
SaurabhRB <saurabhbadenkalat@gmail.com>
2018-04-27 05:04:09 -0700

    -initial commit
    -init commit

---
+++ SimpleHttpServer.WebApp/Routes.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (C) 2016 by Barend Erasmus and donated to the public domain
+
+using SimpleHttpServer.Models;
+using SimpleHttpServer.RouteHandlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHttpServer.WebApp
+{
+    static class Routes
+    {
+
+        public static List<Route> GET
+        {
+            get
+            {
+                return new List<Route>()
+                {
+                    new Route()
+                    {
+                        Callable = HomeIndex,
+                        UrlRegex = "^\\/$",
+                        Method = "GET"
+                    },
+                    new Route()
+                    {
+                        Callable = OS,
+                        UrlRegex = "/OS",
+                        Method = "GET"
+                    },
+                    new Route()
+                    {
+                        Callable = new FileSystemRouteHandler() { BasePath = @"C:\Users\Barend.Erasmus\Desktop\Test"}.Handle,
+                        UrlRegex = "^\\/Static\\/(.*)$",
+                        Method = "GET"
+                    }
+                };
+
+            }
+        }
+
+        private static HttpResponse HomeIndex(HttpRequest request)
+        {
+            return new HttpResponse()
+            {
+                ContentAsUTF8 = "Hello",
+                ReasonPhrase = "OK",
+                StatusCode = "200"
+            };
+
+        }
+
+        private static HttpResponse OS(HttpRequest request)
+        {
+            return new HttpResponse()
+            {
+                Content = {'Project':'Operating System', 'ID' : 'CMPE-180C', 'Team' : '19'},
+                ReasonPhrase = "OK",
+                StatusCode = "200"
+            };
+
+        }
+    }
+}
